4bdcf34c5afa0794ef6f490b6d1dcac0182668cb
luin <i@zihua.li>
2021-04-04 12:13:38 +0800

    Add 3.7

---
+++ 3.7/index.js
@@ -0,0 +1,31 @@
+const pMap = require("p-map");
+const Redis = require("./lib/index");
+const redis = new Redis({ port: 9090, keyPrefix: "book:" });
+
+const RECORD_COUNT = 1000000;
+let gid = 0;
+
+// 使用有序集合+散列类型
+async function insertRecord1() {
+  gid += 1;
+
+  await redis
+    .pipeline()
+    .zadd("visitorList", Date.now(), gid)
+    .hset(`visitorList:${gid}`, "ua", "ua", "ip", "ip")
+    .exec();
+}
+
+// 使用流类型
+async function insertRecord2() {
+  await redis.xadd("visitorList", "*", "ua", "ua", "ip", "ip");
+}
+
+async function main() {
+  await pMap(new Array(RECORD_COUNT).fill(0), insertRecord2, {
+    concurrency: 1000,
+  });
+  redis.quit();
+}
+
+main();
